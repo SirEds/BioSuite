3a96e751be45559eede4089590ec392accb9de24
Edwin Laboy <63374885+SirEds@users.noreply.github.com>
2021-10-11 00:09:53 -0400

    V0

---
+++ Code_Name.py
@@ -0,0 +1,41 @@
+
+
+def protein_code_full(code):
+    amino = {
+        'A':'Alanine',
+        'R':'Arganine',
+        'N':'Asparagine',
+        'D':'Aspartic Acid',
+        'C':'Cysteine',
+        'Q':'Glutamine',
+        'E':'Glutamic Acid',
+        'G':'Glycine',
+        'H':'Histidine',
+        'I':'Isoleucine',
+        'K':'Lysine',
+        'M':'Methionine',
+        'F':'Phenylalanine',
+        'P':'Proline',
+        'S':'Serine',
+        'T':'Threonine',
+        'W':'Tryptophan',
+        'Y':'Tyrosine',
+        'V':'Valine'
+    }
+
+    code_lst = list(code)
+    name_lst = []
+    for i in code_lst:
+        if i in amino:
+            name_lst.append(amino[i])
+    print('---'.join(name_lst))
+
+#input
+
+if __name__ == "__main__":
+    seq = input('''
+    Enter the protein sequence \n\n 
+    *Make sure the sequence does not have space or whitespace(enter)*\n\n
+    '''
+    )
+    protein_code_full(seq)
